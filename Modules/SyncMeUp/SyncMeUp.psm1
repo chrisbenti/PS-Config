48ae270ffb3c3a64708e243a6599bc0f8e244e46
Chris Bentivenga <chris@chrisbenti.com>
2013-08-29 17:34:52 -0700

    Test

--- Modules/SyncMeUp/SyncMeUp.psm1
+++ Modules/SyncMeUp/SyncMeUp.psm1
@@ -1,3 +1,16 @@
+function reload{
+    $process = get-process | ?{$_.ID -eq $pid}
+    if($process.MainWindowTitle.Equals("")){
+        # Console
+        start "C:\Program Files (x86)\Console2\Console.exe"
+    } else {
+        # Powershell 
+        start "powershell.exe"
+    }
+    $process | kill
+
+}
+
 function sync{
     $Start = Get-Location
     $PowerShellConfig = "c:$($env:HOMEPATH)\Documents\WindowsPowershell"
@@ -36,18 +49,5 @@ function sync{
     Set-Location $Start
 
     Write-Host "All Synced!" -f Green
-    if($Reload){ reload() }
-}
-
-function reload{
-    $process = get-process | ?{$_.ID -eq $pid}
-    if($process.MainWindowTitle.Equals("")){
-        # Console
-        start "C:\Program Files (x86)\Console2\Console.exe"
-    } else {
-        # Powershell 
-        start "powershell.exe"
-    }
-    $process | kill
-
+    if($Reload){ reload}
 }
