35ec526a1bed7899ffb72ebdcdac98193fd273ab
Chris Bentivenga <chris@chrisbenti.com>
2013-08-29 17:33:32 -0700

    Adding in autoreloading

--- Modules/SyncMeUp/SyncMeUp.psm1
+++ Modules/SyncMeUp/SyncMeUp.psm1
@@ -2,6 +2,7 @@ function sync{
     $Start = Get-Location
     $PowerShellConfig = "c:$($env:HOMEPATH)\Documents\WindowsPowershell"
     $ConsoleConfig = "c:$($env:HOMEPATH)\Application Data\Console"
+    $Reload = $False
 
     $LookAt = (
             ("Console2 Configuration", $ConsoleConfig),
@@ -12,19 +13,41 @@ function sync{
     $LookAt | % {
         Set-Location $_[1]
         $status = (gst)[-1]
+        $pull = ""
         if($status.Contains("working directory clean")){
-            Write-Host "No local $($_[0])" -f Green 
-            git pull
+            Write-Host "No changes to: $($_[0])" -f Green 
+            $pull = (git pull)
         } else {
-            Write-Host "Syncing $($_[0])" -f Red
+            Write-Host "Syncing: $($_[0])" -f Red
+            git add -u
             git add .
+            Write-Host "Diff Message: " -f Blue
+            git diff 
             git commit -m (Read-Host -Prompt "Commit Message")
-            git pull
+            $pull = (git pull)
             git push
+        }
+
+        if ($pull[0].Contains("Updating")){
+            $Reload = $True
         }
     }
 
     Set-Location $Start
 
     Write-Host "All Synced!" -f Green
+    if($Reload){ reload }
+}
+
+function reload{
+    $process = get-process | ?{$_.ID -eq $pid}
+    if($process.MainWindowTitle.Equals("")){
+        # Console
+        start "C:\Program Files (x86)\Console2\Console.exe"
+    } else {
+        # Powershell 
+        start "powershell.exe"
+    }
+    $process | kill
+
 }
