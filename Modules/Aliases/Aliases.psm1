ff1adfe8293c2a8198c346c0a192d708ce32e89a
Chris Bentivenga <chris@chrisbenti.com>
2013-11-07 15:41:46 -0800

    b

--- Modules/Aliases/Aliases.psm1
+++ Modules/Aliases/Aliases.psm1
@@ -8,3 +8,16 @@ function gst {git status}
 function Colors {
     [ConsoleColor].DeclaredMembers | Select Name | Where {$_.Name -ne "value__" } |% {Write-Host $_.Name -f $_.Name}
 }
+
+function view {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline=$True)]$content
+    )
+
+    $file = $env:temp + "\TEMP-" + [System.Guid]::NewGuid().toString() + ".txt"
+    write-output $content > $file
+    write-host $file
+    & "C:\Program Files\Sublime Text 3\sublime_text.exe" $file
+    start-job -InputObject $file -ScriptBlock{start-sleep 1; rm $input} | out-null
+}
