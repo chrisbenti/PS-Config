05550b9fa79020459179df04a536047fe17e37e8
Chris Bentivenga <chris@chrisbenti.com>
2014-06-11 13:56:55 -0700

    Adds script browser to ISE profile

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,10 @@
+
+#Script Browser Begin
+#Version: 1.2.1
+Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\System.Windows.Interactivity.dll'
+Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\ScriptBrowser.dll'
+Add-Type -Path 'C:\Program Files (x86)\Microsoft Corporation\Microsoft Script Browser\BestPractices.dll'
+$scriptBrowser = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Script Browser', [ScriptExplorer.Views.MainView], $true)
+$scriptAnalyzer = $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add('Script Analyzer', [BestPractices.Views.BestPracticesView], $true)
+$psISE.CurrentPowerShellTab.VisibleVerticalAddOnTools.SelectedAddOnTool = $scriptBrowser
+#Script Browser End
