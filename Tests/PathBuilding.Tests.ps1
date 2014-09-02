82dc3bd15f29913a3af0dd2ed6dfb5d6fa0c1163
Chris Bentivenga <chris@chrisbenti.com>
2014-09-01 23:06:53 -0700

    Fixes and updates for non-FileSystem providers

--- Tests/PathBuilding.Tests.ps1
+++ Tests/PathBuilding.Tests.ps1
@@ -5,25 +5,60 @@ if ($env:CI) {
     Mock Get-Home { return "c:/User/" }
 }
 
+
+Describe "Get-Provider" {
+    Context "Determining the provider" {
+        It "Should handle the FileSystem" {
+            Get-Provider "C:\Windows\System32" | Should Be "FileSystem"
+        }
+
+        It "Should handle Cert:" {
+            Get-Provider "Cert:\CurrentUser\CA" | Should Be "Certificate"
+        }
+
+        It "Should handle SMB shares" {
+            Get-Provider "\\localhost\c$\Windows\System32" | Should Be "FileSystem"
+        }
+
+        It "Should handle the registry" {
+            Get-Provider "HKLM:\SOFTWARE\Microsoft" | Should Be "Registry"
+        }
+    }
+}
+
+
 Describe "UNC Shares" {
     Context "When navigated to a UNC share" {
+        It "Get-Drive returns the name of the share" {
+            Get-Drive "Microsoft.PowerShell.Core\FileSystem::\\localhost\c$\Windows\System32" | Should be "\\localhost\c$\"
+            Get-Drive "Microsoft.PowerShell.Core\FileSystem::\\localhost\c$" | Should be "\\localhost\c$\"
+        }
         It "Shorten-Path returns all parts after the share name" {
             Shorten-Path "\\localhost\c$" | Should Be ""
             Shorten-Path "\\localhost\c$\Windows" | Should be "Windows"
             Shorten-Path "\\localhost\c$\Windows\System32" | Should be "Win\System32"
-        }
-
-        It "Get-Drive returns the name of the share" {
-            Get-Drive "Microsoft.PowerShell.Core\FileSystem::\\localhost\c$\Windows\System32" | Should be "\\localhost\c$"
-            Get-Drive "Microsoft.PowerShell.Core\FileSystem::\\localhost\c$" | Should be "\\localhost\c$"
         }
     }
 }
 
 Describe "Drive Directories" {
     Context "When in the C:\Windows\system32 directory" {
-        It "Returns all parts after the C:\" {
+        It "Get-Drive returns C:\" {
+            Get-Drive "C:\Windows\System32" | Should be "C:\"
+        }
+        It "Shorten-Path returns all parts after the C:\" {
             Shorten-Path "C:\Windows\System32" | Should Be "Win\System32"   
+        }
+    }
+}
+
+Describe "PowerShell non-FileSystem Providers" {
+    Context "When in the cert provider"{
+        It "Get-Drive should return something" {
+            Get-Drive "Cert:\CurrentUser\CA" | Should Be "Cert:\"
+        }
+        It "Shorten-Path should return the rest of the directory" {
+            Shorten-Path "Cert:\CurrentUser\CA" | Should Be "CurrentUser\CA"
         }
     }
 }
